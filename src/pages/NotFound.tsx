d8dd046737bc595ba249bb4a0093f072ef3c2008
wlqrksmswnd <wlqrksmswnd@gmail.com>
2025-09-25 11:50:38 +0900

    404 message changed

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -7,7 +7,7 @@ export default function NotFound() {
             <div id='notfound'>404 Not Found</div>
             <div className='notfound-text_container'>
                 <div id="notfound-title">주인장이 뭔가 실수를 했나봐요.</div>
-                <div id='notfound-text'>곧 해결할 거에요(아마)</div>
+                <div id='notfound-text'>아마 곧 해결할 거에요</div>
             </div>
             
         </div>
