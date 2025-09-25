dcd2947296883c578e62c3d474c22dacab7a43e0
wlqrksmswnd <wlqrksmswnd@gmail.com>
2025-09-25 11:53:54 +0900

    stackcard animation fixed

--- src/components/StackCard.jsx
+++ src/components/StackCard.jsx
@@ -3,12 +3,13 @@ import '../styles/stackcard.css'
 import { delay } from "motion"
 export default function StackCard({stackname}){
     return(
-        <motion.section>
+        <motion.section whileHover={{scale:1.1}}>
             <motion.div className = "stackcard-container"
             initial={{ opacity: 0, y: 20 }}
             whileInView={{ opacity: 1, y: 0 }}
             viewport={{ once: true, amount: 0.6 }}
-            transition={{ duration: 0.6 }}>
+            transition={{ duration: 0.6 }}
+            >
                 <img id="stackcard-stack_img" src={`/stack_imgs/${stackname}.png`}></img>
                 <div id="stackcard-stackname">{stackname}</div>
             </motion.div>
